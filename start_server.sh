8c8c3fe25a4c583bfa2e8bb3e4ede7574e8d5d21
Tkay <um006500@gmail.com>
2020-06-16 17:53:11 +0900

    building recommender notebook

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 # start spark.
-spark-submit --master spark://192.168.56.1:7077 --total-executor-cores 14 --executor-memory 6g server.py
+spark-submit --master spark://192.168.32.129:7077 --total-executor-cores 14 --executor-memory 6g server.py
